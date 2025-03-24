cb67dcda1c0f36c47daa2e71b65c266d31acb0ca
S312User <S312User@S312.uc-bcf.edu.ph>
2025-03-24 11:17:26 +0800

    Included Exercises 3, 4, and 5

--- index.php
+++ index.php
@@ -41,7 +41,86 @@
         } else {
             echo "<p>The number is not even</p>";
         }
+
+        //Exercise 3:
+        echo "Exercise 3: Loops <br>";
+    echo"<br>";
+
+    echo "FizzBuzz:<br>";
+    for ($i = 1; $i <= 100; $i++) {
+        if($i % 3 == 0){
+            if($i % 5 == 0){
+                echo "FizzBuzz, ";
+            }
+            else{
+                echo "Fizz, ";
+            }
+        }
+        elseif($i % 5 == 0){
+            if($i % 3 == 0){
+                echo "FizzBuzz, ";
+            }
+            else{
+                echo "Buzz, ";
+            }
+    }
+    }
+    echo"<br>";
+    echo"<br>";
+
+    echo"Fibonacci:<br>";
+    $n = 10;  
+    $fib = [0, 1]; 
+
+    for ($i = 2; $i < $n; $i++) {
+        $fib[$i] = $fib[$i - 1] + $fib[$i - 2];
+    }
+
+    foreach ($fib as $num) {
+        if ($num % 2 == 0) {
+            echo $num . " ";
+        }
+    }
+    echo"<br>";
+    echo"<br>";
+    echo"<br>";
+?>
+
+    <?php
+
+        //Exercise 4
+    echo "Exercise 4: Functions <br>";
+    echo"<br>";
+    echo"Greeting function:<br>";
+    function greet($name) {
+        return "Good day, " . $name . "! Nice meeting you!";
+    }
+    echo greet("Daryl");
+    echo"<br>";
+    echo"<br>";
+    echo"Square function:<br>";
+
+    function square($num) {
+        return $num * $num;
+    }
+    
+    echo"Square of number 10: ";
+    echo square(10);
+
+    echo"<br>";
+    echo"<br>";
+
+?>
+  
+    <!--Exercise 5-->
+    <h5>Exercise 5: Form Handling <br></h5>
+    <h5>HTML Form:<br></h5>
+    <h2>Enter Some Text:</h2>
+        <form action="repo.php" method="post">
+            <input type="text" name="user_text" required>
+            <button type="submit">Submit</button>
+        </form>
                 
-    ?>
+
 </body>
 </html>
